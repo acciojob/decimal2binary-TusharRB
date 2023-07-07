d597bf75d5a5da2da8d41e3118d587054c4f2c9d
Umakant Dwivedi <78226651+ukdgit@users.noreply.github.com>
2023-07-07 15:15:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,46 +1,5 @@
-function threeSum(arr, target) {
-  const result = [];
-
-  // Sort the array in ascending order
-  arr.sort((a, b) => a - b);
-
-  const n = arr.length;
-
-  for (let i = 0; i < n - 2; i++) {
-    // Skip duplicates
-    if (i > 0 && arr[i] === arr[i - 1]) {
-      continue;
-    }
-
-    let left = i + 1;
-    let right = n - 1;
-
-    while (left < right) {
-      const sum = arr[i] + arr[left] + arr[right];
-
-      if (sum === target) {
-        result.push([arr[i], arr[left], arr[right]]);
-
-        // Skip duplicates
-        while (left < right && arr[left] === arr[left + 1]) {
-          left++;
-        }
-
-        while (left < right && arr[right] === arr[right - 1]) {
-          right--;
-        }
-
-        left++;
-        right--;
-      } else if (sum < target) {
-        left++;
-      } else {
-        right--;
-      }
-    }
-  }
-
-  return result;
+function decimalToBinary(num) {
+  // Write your code here
 }
 
-module.exports = threeSum;
+window.decimalToBinary = decimalToBinary;
